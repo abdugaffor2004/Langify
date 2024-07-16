029c5a11586a65bc32614cc6fdda768e13daadd0
abdugaffor2004 <abdugafforlutfillaev14@gmail.com>
2024-07-16 20:28:38 +0500

    refactor: Убрал не нужные стили. Заменил Object.key на Object.entries

--- src/components/LangSelect/LangSelect.jsx
+++ src/components/LangSelect/LangSelect.jsx
@@ -1,16 +1,14 @@
 import { Select } from '@mantine/core';
 import { LANGUAGES } from '../../data/languages';
-import styles from './LangSelect.module.css';
 
 export const LangSelect = ({ value, onChange }) => (
   <Select
-    data={Object.keys(LANGUAGES).map(key => ({
+    data={Object.entries(LANGUAGES).map(([key, value]) => ({
       value: key,
-      label: LANGUAGES[key],
+      label: value,
     }))}
     value={value}
     onChange={onChange}
-    className={styles.dropdownItem}
     clearable={false}
   />
 );
